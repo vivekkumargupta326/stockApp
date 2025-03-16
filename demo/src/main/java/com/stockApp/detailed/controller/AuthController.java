6d36a99330abb8e78bc213147c3cf505cb7ac562
vivekkumargupta326 <49283335+vivekkumargupta326@users.noreply.github.com>
2025-03-16 23:28:06 +0530

    Added JWT based authentication (#5)
    * Added new sample api
    
    * Update run_test.yml
    
    * Update run_test.yml
    
    * hardcoded api key
    
    * Added JWT based authentication

--- demo/src/main/java/com/stockApp/detailed/controller/AuthController.java
+++ demo/src/main/java/com/stockApp/detailed/controller/AuthController.java
@@ -1,33 +1,18 @@
 package com.stockApp.detailed.controller;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.util.JSONPObject;
-import com.stockApp.detailed.config.RedisConfiguration;
-import com.stockApp.detailed.service.Impl.RedisCacheService;
-import org.json.JSONObject;
+
+import com.stockApp.detailed.security.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/auth")
 public class AuthController {
-
     @Autowired
-    private RedisCacheService redisCacheService;
+    private JwtUtil jwtUtil;
 
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @PostMapping("/auth/get")
-    public ResponseEntity<?> getAuthToken(@RequestHeader String userId) {
-        JSONObject result = new JSONObject();
-        result.put("Token", redisCacheService.generateToken(userId));
-        return new ResponseEntity<>(result.toString(), HttpStatus.OK);
+    @PostMapping("/login")
+    public String login(@RequestHeader("UserId") String userId) {
+        return jwtUtil.generateToken(userId);
     }
 }
---
+++ demo/src/main/java/com/stockApp/detailed/security/JwtUtil.java
@@ -0,0 +1,45 @@
+package com.stockApp.detailed.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
+
+    public String generateToken(String userId) {
+        return Jwts.builder()
+                .setSubject(userId)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractUserId(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    public boolean isTokenValid(String token, String userId) {
+        return (userId.equals(extractUserId(token)) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return getClaims(token).getExpiration().before(new Date());
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY.getBytes()) // Convert String to byte array
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
